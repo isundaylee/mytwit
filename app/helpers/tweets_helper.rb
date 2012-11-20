2d3aeb7acf81fc8a9bd9dbd560f9e389b13baad3
Sunday Lee <isundaylee.reg@gmail.com>
2012-11-20 14:38:42 +0800

    Fixed typo.

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -4,8 +4,9 @@ module TweetsHelper
     content.gsub! /(http:\/\/[^\s]*)/i, '<a href="\1">\1</a>'
     content.gsub! /##([0-9]*)##/i do |id|
       nid = /^##([0-9]*)##\$*/.match(id)[1].to_i
+      logger.debug '------- NID is: ' + nid.to_s
       begin
-        user = User.find!(nid)
+        user = User.find(nid)
         link_to '@' + user.name, show_timeline_path(user)
       rescue
         link_to '@Nobody', '#'
